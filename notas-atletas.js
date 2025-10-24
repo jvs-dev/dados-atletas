4a32d3e62178feba2b0828a15abe3b56fcec10e3
jvs-dev <shepherdcom12@gmail.com>
2025-10-24 02:19:27 -0300

    first commit

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,33 +1,93 @@
-let atletas = [
-   {
-      nome: "Cesar Abascal",
-      notas: [10, 9.34, 8.42, 10, 7.88]
-   },
-   {
-      nome: "Fernando Puntel",
-      notas: [8, 10, 10, 7, 9.33]
-   },
-   {
-      nome: "Daiane Jelinsky",
-      notas: [7, 10, 9.5, 9.5, 8]
-   },
-   {
-      nome: "Bruno Castro",
-      notas: [10, 10, 10, 9, 9.5]
-   }
-];
+class Atleta {
 
-for (let i = 0; i < atletas.length; i++) {
-   let atleta = atletas[i];
-   let nome = atleta.nome;
-   let notasOriginais = atleta.notas;
-   let notasParaExibicao = [...notasOriginais].sort();
-   let notasOrdenadas = [...notasOriginais].sort((a, b) => a - b);
-   let notasComputadas = notasOrdenadas.slice(1, 4);
-   let soma = notasComputadas.reduce((total, nota) => total + nota, 0);
-   let media = soma / notasComputadas.length;
-   console.log(`Atleta: ${nome}`);
-   console.log(`Notas Obtidas: ${notasParaExibicao.join(',')}`);
-   console.log(`Média Válida: ${media}`);
-   console.log("");
+   constructor(nome, idade, peso, altura, notas) {
+      this.nome = nome;
+      this.idade = idade;
+      this.peso = peso;
+      this.altura = altura;
+      this.notas = notas;
+
+      this.categoria = "";
+      this.imc = 0;
+      this.mediaValida = 0;
+
+      this.calculaCategoria();
+      this.calculaIMC();
+      this.calculaMediaValida();
+   }
+
+
+   calculaCategoria() {
+      if (this.idade >= 9 && this.idade <= 11) {
+         this.categoria = "Infantil";
+      } else if (this.idade >= 12 && this.idade <= 13) {
+         this.categoria = "Juvenil";
+      } else if (this.idade >= 14 && this.idade <= 15) {
+         this.categoria = "Intermediário";
+      } else if (this.idade >= 16 && this.idade <= 30) {
+         this.categoria = "Adulto";
+      } else {
+         this.categoria = "Sem categoria";
+      }
+   }
+
+   calculaIMC() {
+      this.imc = this.peso / (this.altura * this.altura);
+   }
+
+   calculaMediaValida() {
+      let notasOrdenadas = [...this.notas].sort((a, b) => a - b);
+
+      let notasComputadas = notasOrdenadas.slice(1, 4);
+
+      let soma = notasComputadas.reduce((total, nota) => total + nota, 0);
+
+      this.mediaValida = soma / notasComputadas.length;
+   }
+
+
+   obtemNomeAtleta() {
+      return this.nome;
+   }
+
+   obtemIdadeAtleta() {
+      return this.idade;
+   }
+
+   obtemPesoAtleta() {
+      return this.peso;
+   }
+
+   obtemAlturaAtleta() {
+      return this.altura;
+   }
+
+   obtemNotasAtleta() {
+      return this.notas;
+   }
+
+   obtemCategoria() {
+      return this.categoria;
+   }
+
+   obtemIMC() {
+      return this.imc;
+   }
+
+   obtemMediaValida() {
+      return this.mediaValida;
+   }
 }
+
+
+const atleta = new Atleta("Cesar Abascal",
+   30, 80, 1.70,
+   [10, 9.34, 8.42, 10, 7.88]);
+console.log(`Nome: ${atleta.obtemNomeAtleta()}`);
+console.log(`Idade: ${atleta.obtemIdadeAtleta()}`);
+console.log(`Peso: ${atleta.obtemPesoAtleta()}`);
+console.log(`Altura: ${atleta.obtemAlturaAtleta()}`);
+console.log(`Notas: ${atleta.obtemNotasAtleta().join(',')}`);
+console.log(`Categoria: ${atleta.obtemCategoria()}`);
+console.log(`IMC: ${atleta.obtemIMC()}`);
+console.log(`Média válida: ${atleta.obtemMediaValida()}`);
